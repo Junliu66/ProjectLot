6fe94b0f0cf1b4922ce645171ab77dcfb2094454
Junliu Zhang <junliuzhang@junlius-mbp.attlocal.net>
2020-05-09 03:23:44 -0700

    first commit

---
+++ ProjectLot/src/main/java/junliuzhang/com/example/projectlot/ProjectLotApplication.java
@@ -0,0 +1,13 @@
+package junliuzhang.com.example.projectlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLotApplication.class, args);
+    }
+
+}
---
+++ ProjectLot/src/test/java/junliuzhang/com/example/projectlot/ProjectLotApplicationTests.java
@@ -0,0 +1,13 @@
+package junliuzhang.com.example.projectlot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
