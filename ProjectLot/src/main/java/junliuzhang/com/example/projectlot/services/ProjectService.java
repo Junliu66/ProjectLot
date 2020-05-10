12be04e00a7b6bdd8c954bb014a13018987bb8e8
Junliu Zhang <junliuzhang@junlius-mbp.attlocal.net>
2020-05-10 01:35:02 -0700

    exception handle duplicate identifier

--- ProjectLot/src/main/java/junliuzhang/com/example/projectlot/services/ProjectService.java
+++ ProjectLot/src/main/java/junliuzhang/com/example/projectlot/services/ProjectService.java
@@ -1,6 +1,7 @@
 package junliuzhang.com.example.projectlot.services;
 
 import junliuzhang.com.example.projectlot.domain.Project;
+import junliuzhang.com.example.projectlot.exceptions.ProjectIdException;
 import junliuzhang.com.example.projectlot.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
 
-        return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 }
