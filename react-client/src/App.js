8c87b6b5811bc235f5f053982b030e246fcf343e
Junliu Zhang <junliuzhang@junlius-mbp.attlocal.net>
2020-05-11 17:15:52 -0700

    react components

---
+++ react-client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}*/
+
+export default App;
